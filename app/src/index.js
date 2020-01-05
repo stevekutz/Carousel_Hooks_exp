26e531999417bd4aa6fe81a40e732d7aaf9b46d5
stevekutz <stkutz@gmail.com>
2020-01-04 21:36:54 -0600

    added menu nav with Routes

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {StateInspector} from 'reinspect';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppWrapper = () => {
+    return (
+        <StateInspector>
+            <Router>
+                <App/>
+            </Router>
+        </StateInspector>
+    ) 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
+
+ReactDOM.render(<AppWrapper />, document.getElementById('root'));
+
